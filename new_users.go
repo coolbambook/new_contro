e4a92e3ed64030ce32c28f1eed05d9d1c65c5686
coolbambook <sydykdana10@gmail.com>
2024-10-14 23:17:28 +0600

    Add files via upload

--- new_users.go
+++ new_users.go
@@ -14,6 +14,7 @@ type Users struct {
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
 	u.Templates.New.Execute(w, nil)
 }
+
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<p>Email: %s</p>", r.FormValue("email"))
 	fmt.Fprintf(w, "<p>Password: %s</p>", r.FormValue("password"))
